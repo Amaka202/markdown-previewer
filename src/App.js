422d2804b53a96f3c4890c1e0496494e54b120c3
Amaka Umeh <amakaumeh01@gmail.com>
2020-12-08 14:27:15 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import "./App.css"
+import Quote from './Quote'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Quote />
+      </div>
+    )
+  }
+}
